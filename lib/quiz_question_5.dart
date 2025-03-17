bedef86d3e6bc0c1e008e057f5258e4ac8f51437
Harsh Zota <hzota1042@gmail.com>
2025-03-17 13:43:32 -0700

    harsh q 2

---
+++ lib/quiz_question_5.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
+import 'dart:math';
+
+void main() {
+  runApp(WaterCycleQuizApp());
+}
+
+class WaterCycleQuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: QuizScreen(),
+    );
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  String feedbackText = '';
+  Color feedbackColor = Colors.black;
+  Color? buttonColor;
+  late ConfettiController _confettiController;
+
+  @override
+  void initState() {
+    super.initState();
+    _confettiController = ConfettiController(duration: Duration(seconds: 3));
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  void checkAnswer(String answer) {
+    if (answer == 'evaporation') {
+      setState(() {
+        feedbackText = 'Correct! That\'s right!';
+        feedbackColor = Colors.green;
+      });
+      _confettiController.play();
+    } else {
+      setState(() {
+        feedbackText = 'Try again!';
+        feedbackColor = Colors.red;
+        buttonColor = Colors.red;
+      });
+      Future.delayed(Duration(seconds: 3), () {
+        setState(() {
+          buttonColor = null;
+        });
+      });
+    }
+  }
+
+  void resetQuiz() {
+    setState(() {
+      feedbackText = '';
+      feedbackColor = Colors.black;
+      buttonColor = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Stack(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Identify the process',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.network(
+                        'https://creatie.ai/ai/api/search-image?query=A%20beautiful%20and%20educational%20illustration%20showing%20the%20water%20cycle%20process,%20specifically%20focusing%20on%20evaporation.',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 3,
+                    children: [
+                      buildAnswerButton('Evaporation', 'evaporation'),
+                      buildAnswerButton('Sublimation', 'sublimation'),
+                      buildAnswerButton('Condensation', 'condensation'),
+                      buildAnswerButton('Freezing', 'freezing'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    feedbackText,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: feedbackColor),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: resetQuiz,
+                    child: Text('Next Question'),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirection: -pi / 2,
+              emissionFrequency: 0.05,
+              numberOfParticles: 20,
+              gravity: 0.2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildAnswerButton(String text, String answer) {
+    return ElevatedButton(
+      onPressed: () => checkAnswer(answer),
+      child: Text(text, style: TextStyle(fontSize: 16)),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        backgroundColor: buttonColor != null && answer != 'evaporation' ? buttonColor : Colors.blue,
+      ),
+    );
+  }
+}
